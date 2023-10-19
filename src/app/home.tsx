5883180fa42bd4e3501a9aa7e13420ed224b612c
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-10-19 19:22:25 +0200

    React strcuture clean-up

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,14 +1,10 @@
+import { Loader } from '@aztec/aztec-ui';
+import { CompleteAddress } from '@aztec/aztec.js';
+import { useState } from 'react';
 import { PXE_URL } from '../config.js';
 import { WalletDropdown } from './components/wallet_dropdown.js';
 import { Contract } from './contract.js';
-import { DeveloperMode } from './modals/developer_mode.js';
 import styles from './home.module.scss';
-import { Loader } from '@aztec/aztec-ui';
-import { CompleteAddress } from '@aztec/aztec.js';
-import { useEffect, useRef, useState } from 'react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 export function Home() {
   const [isLoadingWallet, setIsLoadingWallet] = useState(true);
@@ -26,31 +22,29 @@ export function Home() {
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Quetzal</h1>
-        <>
-          {isLoadingWallet && <Loader />}
-          {!isLoadingWallet && (
-            <>
-              {!!selectWalletError && (
-                <>
-                  {`Failed to load accounts. Error: ${selectWalletError}`}
-                  <br />
-                  {`Make sure PXE from Aztec Sandbox is running at: ${PXE_URL}`}
-                </>
-              )}
-              {!selectWalletError && !selectedWallet && `No accounts.`}
-              {!selectWalletError && !!selectedWallet && <Contract wallet={selectedWallet} />}
-            </>
-          )}
-          <WalletDropdown
-            selected={selectedWallet}
-            onSelectChange={handleSelectWallet}
-            onError={handleSelectWalletError}
-          />
-        </>
-      </main>
-    </QueryClientProvider>
+    <main className={styles.main}>
+      <h1 className="text-3xl font-bold underline">Quetzal</h1>
+      <>
+        {isLoadingWallet && <Loader />}
+        {!isLoadingWallet && (
+          <>
+            {!!selectWalletError && (
+              <>
+                {`Failed to load accounts. Error: ${selectWalletError}`}
+                <br />
+                {`Make sure PXE from Aztec Sandbox is running at: ${PXE_URL}`}
+              </>
+            )}
+            {!selectWalletError && !selectedWallet && `No accounts.`}
+            {!selectWalletError && !!selectedWallet && <Contract wallet={selectedWallet} />}
+          </>
+        )}
+        <WalletDropdown
+          selected={selectedWallet}
+          onSelectChange={handleSelectWallet}
+          onError={handleSelectWalletError}
+        />
+      </>
+    </main>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,15 @@ import { Home } from './home.js';
 import './index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <Home />
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
