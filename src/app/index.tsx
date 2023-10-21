f7eca8befe05f36d743cfdd2c5c8a42b5874c6cd
Arkadiusz Konior <akonior@gmail.com>
2023-10-21 09:00:10 +0200

    Generate noir parameters during registration and login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import './index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import {QueryClient} from '@tanstack/react-query';
+import {decode} from 'cbor-x';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -9,14 +10,14 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const register = async () => {
     const publicKeyCredentialCreationOptions: any = {
         rp: {
-            name: "Quezal test",
+            name: "Quezal smart contract",
         },
         user: {
             id: new Uint8Array(16),
-            name: "test",
-            displayName: "test",
+            name: "test@zkworks.xyz",
+            displayName: "Test",
         },
-        challenge: new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        challenge: new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]).buffer,
         pubKeyCredParams: [{type: "public-key", alg: -7}],
         timeout: 60000,
         attestation: "direct",
@@ -26,47 +27,67 @@ const register = async () => {
         "publicKey": publicKeyCredentialCreationOptions
     });
 
-    console.log(credential);
-
-    // TODO:
-    // 1. Extract (x, y) values from public key
-    // 2. save user id => (x, y)
+    const decodedAttestationObj = decode(new Uint8Array(credential.response.attestationObject));
+    const {authData} = decodedAttestationObj;
+    const dataView = new DataView(new ArrayBuffer(2));
+    const idLenBytes = authData.slice(53, 55);
+    idLenBytes.forEach((value: any, index: any) => dataView.setUint8(index, value));
+    // @ts-ignore
+    const credentialIdLength = dataView.getUint16();
+    const publicKeyBytes = authData.slice(55 + credentialIdLength);
+    const publicKeyObject = decode(new Uint8Array(publicKeyBytes.buffer));
+    const x = publicKeyObject[-2];
+    const y = publicKeyObject[-3];
+    const pub_key_x_str = `pub_key_x = [${x}]`;
+    const pub_key_y_str = `pub_key_y = [${y}]`;
+    const noir_parameters = [pub_key_x_str, pub_key_y_str].join('\n');
+    console.log(noir_parameters);
 }
 
 const login = async () => {
+    let challenge = new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]).buffer;
     const credentialRequestOptions = {
         publicKey: {
             timeout: 60000,
-            allowCredentials: [],	 // see below
-            challenge: new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).buffer,
+            allowCredentials: [],
+            challenge: challenge,
         }
     };
 
     const assertation: any = await navigator.credentials.get(
         credentialRequestOptions
     );
+    const challenge_str = `challenge = [${toBase64url(challenge).replaceAll('=', '').split('').map((c) => c.charCodeAt(0))}}]`;
+    const authenticator_data_str = `authenticator_data = [${new Uint8Array((assertation.response.authenticatorData))}]`;
+    const client_data_json_str = `client_data_json = [${new Uint8Array((assertation.response.clientDataJSON))}]`;
+    let signatureRaw = convertASN1toRaw(assertation.response.signature);
+    const signature_str = `signature = [${new Uint8Array(signatureRaw)}]`;
 
-    const enc = new TextEncoder()
-    console.log(assertation);
-    console.log(`authenticatorData: ${enc.encode(assertation.response.authenticatorData)}`);
-    console.log(`clientDataJSON: ${enc.encode(assertation.response.clientDataJSON)}`);
-    const utf8Decoder = new TextDecoder('utf-8');
-    const decodedClientData = utf8Decoder.decode(assertation.response.clientDataJSON)
-    console.log(`clientDataJSON: ${decodedClientData}`);
-
-    const hash = await crypto.subtle.digest('SHA-256', assertation.response.clientDataJSON);
-    console.log(`hash: ${arrayBufferToString(hash)}`);
-
-    const signedData = assertation.response.authenticatorData + hash;
-    console.log(`signedData: ${enc.encode(signedData)}`)
-    console.log(`signature: ${enc.encode(assertation.response.signature)}`)
-
-    // TODO: check verifySignature(publicKey, signedData, signature)
-    // https://noir-lang.org/standard_library/cryptographic_primitives/ecdsa_sig_verification#ecdsa_secp256r1verify_signature
+    console.log([challenge_str, authenticator_data_str, client_data_json_str, signature_str].join('\n'));
 };
 
 function arrayBufferToString(buffer: ArrayBuffer) {
     return new Uint8Array(buffer).toString();
+}
+
+export function toBase64url(buffer: ArrayBuffer) {
+    const txt = btoa(parseBuffer(buffer)); // base64
+    return txt.replaceAll('+', '-').replaceAll('/', '_');
+}
+
+export function parseBuffer(buffer: ArrayBuffer) {
+    return String.fromCharCode(...new Uint8Array(buffer));
+}
+
+function convertASN1toRaw(signatureBuffer: ArrayBuffer) {
+    // Convert signature from ASN.1 sequence to "raw" format
+    const usignature = new Uint8Array(signatureBuffer);
+    const rStart = usignature[4] === 0 ? 5 : 4;
+    const rEnd = rStart + 32;
+    const sStart = usignature[rEnd + 2] === 0 ? rEnd + 3 : rEnd + 2;
+    const r = usignature.slice(rStart, rEnd);
+    const s = usignature.slice(sStart);
+    return new Uint8Array([...r, ...s]);
 }
 
 root.render(
