9aaebc3a72199f50f1fe1ce2fdc8a68832c46f71
Arkadiusz Konior <akonior@gmail.com>
2023-10-23 11:35:23 +0200

    correct spaces formatting

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,16 @@
+import { Home } from './home.js';
 import './index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {Home} from "./home.js";
-import WebAuthnRegister from "./components/webauthn/register.js";
-import WebAuthnLogin from "./components/webauthn/login.js";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Home/>
-            <WebAuthnRegister/>
-            <WebAuthnLogin/>
-        </QueryClientProvider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
