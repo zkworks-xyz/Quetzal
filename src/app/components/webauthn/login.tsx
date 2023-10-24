91dfbe354f92057d468361d1001747803a3737ff
Arkadiusz Konior <akonior@gmail.com>
2023-10-24 09:47:38 +0200

    Webauth React components and Noir proof/verification
    * todo
    
    * Passkey login call
    
    * Extract signed data and signature
    
    * Link to noir signature verification
    
    * move token contract
    
    * Add WebAuthn contract
    
    * Webauthn signature test data
    
    * Webauthn signature verification contract
    
    * Add challenge validation.
    
    * Generate noir parameters during registration and login
    
    * New noir parameters
    
    * Noir prover and verifier data
    
    * Extract register and login to separate components
    
    * CI webauthn contract test configuration
    
    * yarn.lock update
    
    * register refactor: separate webauthn api call from logging
    
    * login refactor: separate webauthn api call from logging
    
    * correct spaces formatting
    
    * Ci nargo build path fix
    
    * Ci nargo cli fix
    
    * Ci nargo webauth remove prove/verify
    
    * yarn.lock update cbor dependency

---
+++ src/app/components/webauthn/login.tsx
@@ -0,0 +1,62 @@
+import {Button} from "@aztec/aztec-ui";
+import * as React from "react";
+
+async function webAuthnLogin(challenge: Uint8Array) {
+    const credentialRequestOptions = {
+        publicKey: {
+            timeout: 60000,
+            allowCredentials: [],
+            challenge: challenge,
+        }
+    };
+
+    // @ts-ignore
+    const assertation: any = await navigator.credentials.get(
+        credentialRequestOptions
+    );
+    // @ts-ignore
+    const challenge1 = toBase64url(challenge).replaceAll('=', '').split('').map((c) => c.charCodeAt(0));
+    const authenticator_data = new Uint8Array((assertation.response.authenticatorData));
+    const client_data_json = new Uint8Array((assertation.response.clientDataJSON));
+    let signatureRaw = convertASN1toRaw(assertation.response.signature);
+    return {challenge: challenge1, authenticator_data, client_data_json, signatureRaw};
+}
+
+const login = async () => {
+    let {challenge, authenticator_data, client_data_json, signatureRaw} =
+        await webAuthnLogin(new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]));
+
+    const challenge_str = `challenge = [${challenge}}]`;
+    const client_data_json_str = `client_data_json = [${client_data_json}]`;
+    const authenticator_data_str = `authenticator_data = [${authenticator_data}]`;
+    const signature_str = `signature = [${new Uint8Array(signatureRaw)}]`;
+
+    console.log([challenge_str, authenticator_data_str, client_data_json_str, signature_str].join('\n'));
+};
+
+function toBase64url(buffer: ArrayBuffer): string {
+    const txt: string = btoa(parseBuffer(buffer)); // base64
+    // @ts-ignore
+    return txt.replaceAll('+', '-').replaceAll('/', '_');
+}
+
+function parseBuffer(buffer: ArrayBuffer) {
+    return String.fromCharCode(...new Uint8Array(buffer));
+}
+
+function convertASN1toRaw(signatureBuffer: ArrayBuffer) {
+    // Convert signature from ASN.1 sequence to "raw" format
+    const usignature = new Uint8Array(signatureBuffer);
+    const rStart = usignature[4] === 0 ? 5 : 4;
+    const rEnd = rStart + 32;
+    const sStart = usignature[rEnd + 2] === 0 ? rEnd + 3 : rEnd + 2;
+    const r = usignature.slice(rStart, rEnd);
+    const s = usignature.slice(sStart);
+    return new Uint8Array([...r, ...s]);
+}
+
+export default function WebAuthnLogin() {
+    return (
+        <Button text={'Sign transaction'} onClick={login}/>
+    );
+}
---
+++ src/app/components/webauthn/register.tsx
@@ -0,0 +1,58 @@
+import {Button} from "@aztec/aztec-ui";
+import * as React from "react";
+
+import {decode} from "cbor-x";
+
+async function webAuthnFetchPublicKey(challenge: ArrayBuffer): Promise<{ x: any, y: any }> {
+    const publicKeyCredentialCreationOptions: any = {
+        rp: {
+            name: "Quezal smart contract",
+        },
+        user: {
+            id: new Uint8Array(16),
+            name: "test@zkworks.xyz",
+            displayName: "Test",
+        },
+        challenge: challenge,
+        pubKeyCredParams: [{type: "public-key", alg: -7}],
+        allowCredentials: [
+            {type: "public-key", transports: ["internal"]},
+            // ... more Credential IDs can be supplied.
+        ],
+        timeout: 60000,
+        attestation: "direct",
+    };
+
+    // @ts-ignore
+    const credential: any = await navigator.credentials.create({
+        "publicKey": publicKeyCredentialCreationOptions
+    });
+
+    const decodedAttestationObj = decode(new Uint8Array(credential.response.attestationObject));
+    const {authData} = decodedAttestationObj;
+    const dataView = new DataView(new ArrayBuffer(2));
+    const idLenBytes = authData.slice(53, 55);
+    idLenBytes.forEach((value: any, index: any) => dataView.setUint8(index, value));
+    // @ts-ignore
+    const credentialIdLength = dataView.getUint16();
+    const publicKeyBytes = authData.slice(55 + credentialIdLength);
+    const publicKeyObject = decode(new Uint8Array(publicKeyBytes.buffer));
+    const x = publicKeyObject[-2];
+    const y = publicKeyObject[-3];
+    return {x, y};
+}
+
+const register = async () => {
+    const challenge = new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]);
+    const {x, y} = await webAuthnFetchPublicKey(challenge);
+    const pub_key_x_str = `pub_key_x = [${x}]`;
+    const pub_key_y_str = `pub_key_y = [${y}]`;
+    const noir_parameters = [pub_key_x_str, pub_key_y_str].join('\n');
+    console.log(noir_parameters);
+}
+
+export default function WebAuthnRegister() {
+    return (
+        <Button text={'Create account'} onClick={register}/>
+    );
+}
