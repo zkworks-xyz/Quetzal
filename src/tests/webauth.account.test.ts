e2961d2772b07b6fb35fa2480d989706b1d5b638
Arkadiusz Konior <akonior@gmail.com>
2023-10-25 15:48:18 +0200

    Basic WebAuthn account create, deploy and check deployed test

--- src/tests/webauth.account.test.ts
+++ src/tests/webauth.account.test.ts
@@ -1,7 +1,39 @@
 import { beforeAll, describe, expect, it } from "@jest/globals";
 import { setupSandbox } from "./utils.js";
-import { Fr, getSchnorrAccount, isContractDeployed, PXE } from "@aztec/aztec.js";
+import {
+  AccountManager, AccountWalletWithPrivateKey,
+  AuthWitnessProvider,
+  BaseAccountContract,
+  CompleteAddress,
+  Fr,
+  getSchnorrAccount,
+  isContractDeployed,
+  Point,
+  PXE
+} from "@aztec/aztec.js";
 import { GrumpkinScalar } from "@aztec/circuits.js";
+import { WebAuthnAccountContractArtifact } from "../contracts/artifacts/webauthn/WebAuthnAccount.js";
+import { AuthWitness } from "@aztec/types";
+
+class WebAuthnAccountContract extends BaseAccountContract {
+  constructor() {
+    super(WebAuthnAccountContractArtifact);
+  }
+
+  getDeploymentArgs(): Promise<any[]> {
+    const publicKey = Point.ZERO;
+    return Promise.resolve([publicKey.x, publicKey.y]);
+  }
+
+  getAuthWitnessProvider(_address: CompleteAddress): AuthWitnessProvider {
+    return {
+      async createAuthWitness(message: Fr): Promise<AuthWitness> {
+        const signature: any[] = [] // TODO generate webauthn signature
+        return new AuthWitness(message, [...signature]);
+      },
+    };
+  }
+}
 
 describe("Quetzal wallet", () => {
 
@@ -12,11 +44,21 @@ describe("Quetzal wallet", () => {
   }, 60_000);
 
   it("should deploy SchnorrAccount contract and check deployment status", async () => {
-    const encryptionPrivateKey = GrumpkinScalar.random();
-    const signingPrivateKey = GrumpkinScalar.random();
-    const salt = Fr.ZERO;
-    const schnorrAccount = getSchnorrAccount(pxe, encryptionPrivateKey, signingPrivateKey, salt);
-    let accountWallet = await schnorrAccount.waitDeploy();
+    const encryptionPrivateKey: GrumpkinScalar = GrumpkinScalar.random();
+    const signingPrivateKey: GrumpkinScalar = GrumpkinScalar.random();
+    const salt: Fr = Fr.ZERO;
+    const schnorrAccount: AccountManager = getSchnorrAccount(pxe, encryptionPrivateKey, signingPrivateKey, salt);
+    let accountWallet: AccountWalletWithPrivateKey = await schnorrAccount.waitDeploy();
+
+    const isDeployed = await isContractDeployed(pxe, accountWallet.getAddress())
+    expect(isDeployed).toBe(true);
+  }, 60_000);
+
+  it("should deploy WebAuthnAccount contract and check deployment status", async () => {
+    const encryptionPrivateKey: GrumpkinScalar = GrumpkinScalar.random();
+    const salt: Fr = Fr.ZERO;
+    const webAuthnAccount: AccountManager = new AccountManager(pxe, encryptionPrivateKey, new WebAuthnAccountContract(), salt);
+    let accountWallet: AccountWalletWithPrivateKey = await webAuthnAccount.waitDeploy();
 
     const isDeployed = await isContractDeployed(pxe, accountWallet.getAddress())
     expect(isDeployed).toBe(true);
