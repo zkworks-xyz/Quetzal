4544b2eff923bc557d9cefa73da0c8a8fbfb9d36
Arkadiusz Konior <akonior@gmail.com>
2023-10-26 22:15:42 +0200

    using stud public key

--- src/app/account/webauthn_account_contract.ts
+++ src/app/account/webauthn_account_contract.ts
@@ -55,11 +55,7 @@ export class WebAuthnAccountContract extends BaseAccountContract {
 
   async getDeploymentArgs(): Promise<any[]> {
     const publicKey = await this.webAuntnInterface.getPublicKey();
-    // return Promise.resolve([publicKey.x, publicKey.y]);
-    return Promise.resolve([
-      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-    ]);
+    return Promise.resolve([publicKey.x, publicKey.y]);
   }
 
   getAuthWitnessProvider(_address: CompleteAddress): AuthWitnessProvider {
