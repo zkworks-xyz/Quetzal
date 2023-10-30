fd571095e9898f253c7b765cba14a4da9e89a488
Arkadiusz Konior <akonior@gmail.com>
2023-10-30 15:39:40 +0100

    Uint8Array and bigint conversions

--- src/app/components/webauthn/webauthn.test.ts
+++ src/app/components/webauthn/webauthn.test.ts
@@ -10,9 +10,12 @@ describe('webauthn utils', () => {
 
 // write test fot convertBigIntToUint8Array
   it('convert bigint to u8array', () => {
+    expect(convertBigIntToUint8Array(1n)).toEqual(new Uint8Array([1]));
+    expect(convertBigIntToUint8Array(1n, 4)).toEqual(new Uint8Array([0, 0, 0, 1]));
     expect(convertBigIntToUint8Array(258n)).toEqual(new Uint8Array([0x01, 0x02]));
-    // expect(convertBigIntToUint8Array(16711850n)).toEqual(new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 170, 255]));
-    // expect(convertBigIntToUint8Array(21888242871839275222246405745257275088548364400416034343698204186575808495617n - 1n))
-    //   .toEqual(new Uint8Array([48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51, 232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0]));
+    expect(convertBigIntToUint8Array(258n, 4)).toEqual(new Uint8Array([0, 0, 0x01, 0x02]));
+    expect(convertBigIntToUint8Array(16711850n, 32)).toEqual(new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 170]));
+    expect(convertBigIntToUint8Array(21888242871839275222246405745257275088548364400416034343698204186575808495617n - 1n))
+      .toEqual(new Uint8Array([48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51, 232, 72, 121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 0]));
   });
 })
