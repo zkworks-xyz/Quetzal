b31484c8b1b6ac25c1fdddac2f8fbfdc246f317a
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-10-30 11:49:52 +0100

    Skeleton for Create accounts

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -9,9 +9,6 @@ import { UserAccount } from './model/UserAccount.js';
 import { CreateAccount } from './modals/CreateAccount.js';
 import { Main } from './modals/Main.js';
 
-
-
-
 export function Home() {
   const [account, setAccount] = useState<UserAccount | null>(null);
   return (
