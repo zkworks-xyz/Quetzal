96a6794c515a38920c84ef4e91829fb05605e807
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-10-31 12:29:13 +0100

    Fix input paddings

--- src/app/modals/SentTokens.tsx
+++ src/app/modals/SentTokens.tsx
@@ -27,7 +27,7 @@ export function SendTokens({ account, onClose }: SendTokensProps) {
               name="email"
               type="email"
               autoComplete="email"
-              className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 bg-white/5 px-2 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
@@ -41,7 +41,7 @@ export function SendTokens({ account, onClose }: SendTokensProps) {
               name="first-name"
               id="first-name"
               autoComplete="given-name"
-              className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 bg-white/5 px-2 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
