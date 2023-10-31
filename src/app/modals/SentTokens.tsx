8bdd3e3602f66770f2af33fc4c98a645decda66b
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-10-31 12:30:50 +0100

    Fix dialog paddings

--- src/app/modals/SentTokens.tsx
+++ src/app/modals/SentTokens.tsx
@@ -9,7 +9,7 @@ export function SendTokens({ account, onClose }: SendTokensProps) {
   const sendTokens = async () => {};
 
   return (
-    <section className="bg-white dark:bg-gray-900 max-w-2xl rounded-lg px-8 py-16">
+    <section className="bg-white dark:bg-gray-900 max-w-2xl rounded-lg px-8 py-8">
       <div className="container flex flex-col items-center justify-center px-6 mx-auto">
         <h1 className="mt-4 text-2xl font-semibold tracking-wide text-center text-gray-800 md:text-3xl dark:text-white">
           Send tokens
