37c25c923a7c6b3d1e09946ea7e2a83d12cfcf01
Arkadiusz Konior <akonior@gmail.com>
2023-10-31 11:20:50 +0100

    Show balance

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,21 +1,21 @@
-import { Loader } from '@aztec/aztec-ui';
-import { CompleteAddress } from '@aztec/aztec.js';
 import { useState } from 'react';
-import { PXE_URL } from '../config.js';
-import { WalletDropdown } from './components/wallet_dropdown.js';
-import { Contract } from './contract.js';
 import styles from './home.module.scss';
 import { UserAccount } from './model/UserAccount.js';
 import { CreateAccount } from './modals/CreateAccount.js';
 import { Main } from './modals/Main.js';
+import { TokenContract } from "./account/token.js";
 
 export function Home() {
   const [account, setAccount] = useState<UserAccount | null>(null);
+  const [tokenContract, setTokenContract] = useState<TokenContract | null>(null);
   return (
     <main className={styles.main}>
       <h1 className="text-3xl font-bold underline">Quetzal</h1>
-      {account && <Main account={account}/>}
-      {!account && <CreateAccount onAccountCreated={setAccount}/>}
+      {account && tokenContract && <Main account={account} tokenContract={tokenContract}/>}
+      {!account && <CreateAccount onAccountCreated={(account, tokenContract) => {
+        setAccount(account);
+        setTokenContract(tokenContract);
+      }}/>}
 
       {/* <>
         {isLoadingWallet && <Loader />}
