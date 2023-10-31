49ddcd1e823fc8f670dfc28e312b9f2e3ac4c48b
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-10-31 11:26:20 +0100

    Show shorten address on main screen

--- src/app/modals/Main.tsx
+++ src/app/modals/Main.tsx
@@ -14,7 +14,10 @@ export function Main({ account }: MainProps) {
 
   const refresh = () => {};
 
-  const copy = () => {};
+  const copy = () => {
+    const value = account.account.getAddress().toString();
+    navigator.clipboard.writeText(value);
+  };
 
   return showModal ? (
     <SendTokens account={account} onClose={() => setShowModal(false)} />
@@ -22,7 +25,7 @@ export function Main({ account }: MainProps) {
     <section className="bg-white dark:bg-gray-900 max-w-2xl rounded-lg px-8 py-8 flex-auto flex-col">
       <div className="mt-6 text-gray-500 dark:text-gray-400 text-base text-left">Your address</div>
       <div className="mt-1 text-gray-800 md:text-xl dark:text-white text-base text-left flex-col">
-        {account.address}
+        {account.account.getAddress().toShortString()}
         <button
           onClick={() => copy()}
           className="px-3 py-1 ml-4 text-xs font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
