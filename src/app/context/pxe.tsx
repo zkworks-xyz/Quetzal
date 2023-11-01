28c328886cbd61118010a361d12e2247fbed7070
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-11-01 20:38:55 +0100

    Introdcue Config context

--- src/app/context/pxe.tsx
+++ src/app/context/pxe.tsx
@@ -1,34 +1,34 @@
 import { PXE, createPXEClient, waitForSandbox } from '@aztec/aztec.js';
-import React from 'react';
+import { useEffect, useState, createContext, ReactNode, useContext } from 'react';
 import { WaitDialog } from '../modals/WaitDialog.js';
+import { useConfig } from './config.js';
 
-export const PXEContext = React.createContext<PXE | null>(null);
+export const PXEContext = createContext<PXE | null>(null);
 
-export const PXEProvider = ({ children } : {children: React.ReactNode}) => {
-    const [pxe, setPXE] = React.useState<PXE | null>(null);
+export const PXEProvider = ({ children }: { children: ReactNode }) => {
+  const [pxe, setPXE] = useState<PXE | null>(null);
+  const { PXE_URL } = useConfig();
 
-    React.useEffect(() => {
-        const init = async () => {
-            const { PXE_URL = 'http://localhost:8081' } = process.env;
-            const pxe = createPXEClient(PXE_URL);
-            await waitForSandbox(pxe);
-            setPXE(pxe);
-        }
-        init();
-    }, []);
+  useEffect(() => {
+    const init = async () => {
+      const pxe = createPXEClient(PXE_URL);
+      await waitForSandbox(pxe);
+      setPXE(pxe);
+    };
+    init();
+  }, []);
 
-
-    return pxe ? (
-        <PXEContext.Provider value={pxe}>
-            {children}
-        </PXEContext.Provider>
-    ) : <WaitDialog message="Can't connect to PXE"/>;
-}
+  return pxe ? (
+    <PXEContext.Provider value={pxe}>{children}</PXEContext.Provider>
+  ) : (
+    <WaitDialog message="Can't connect to PXE" />
+  );
+};
 
 export const usePXE = () => {
-    const pxe = React.useContext(PXEContext);
-    if (!pxe) {
-        throw new Error('usePXE must be used within a PXEProvider');
-    }
-    return pxe;
-}
+  const pxe = useContext(PXEContext);
+  if (!pxe) {
+    throw new Error('usePXE must be used within a PXEProvider');
+  }
+  return pxe;
+};
