676c3e3cec26477f0421918e7c91f1cd17844aa5
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-11-03 16:37:03 +0100

    Use useQuery in Main

--- src/app/modals/Main.tsx
+++ src/app/modals/Main.tsx
@@ -1,45 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { UserAccount } from '../model/UserAccount.js';
 import { SendTokens } from './SendTokens.js';
 import { TokenContract } from '@aztec/noir-contracts/types';
+import { useQuery } from '@tanstack/react-query';
 import { TOKEN_LIST } from '../model/token_list.js';
 
 interface MainProps {
   account: UserAccount;
 }
 
+enum CurrentModal {
+  Main,
+  SendTokens,
+}
+
 export function Main({ account }: MainProps) {
-  const [showModal, setShowModal] = useState<boolean>(false);
-  const [balance, setBalance] = useState<string | null>(null);
+  const [modal, setModal] = useState<CurrentModal>(CurrentModal.Main);
   const [tokenContract, setTokenContract] = useState<TokenContract | null>(null);
 
-  const refresh = async () => {
-    console.log('Check balance...');
-
+  const fetchBalance = async () => {
     const token = TOKEN_LIST[0];
     const tokenContract = await TokenContract.at(token.address, account.account);
     setTokenContract(tokenContract);
 
-    const balance = await tokenContract.methods.balance_of_public(account.account.getAddress()).view();
-    console.log(`Check balance DONE. balance: ${balance.toString()}`);
-    setBalance(balance.toString());
+    return tokenContract.methods.balance_of_public(account.account.getAddress()).view();
   };
 
-  useEffect(() => {
-    const fetchBalance = async () => {
-      await refresh();
-    };
-    fetchBalance().catch(console.error);
-  }, []);
+  const { data, isError, isPending, refetch } = useQuery({ queryKey: ['balance'], queryFn: fetchBalance });
 
   const copy = () => {
     const value = account.account.getAddress().toString();
     navigator.clipboard.writeText(value);
   };
 
-  return showModal ? (
-    <SendTokens account={account} tokenContract={tokenContract!} onClose={() => setShowModal(false)} onSuccess={() => setShowModal(false)} />
-  ) : (
+  if (CurrentModal.SendTokens === modal) {
+    return (
+      <SendTokens
+        account={account}
+        tokenContract={tokenContract!}
+        onClose={() => setModal(CurrentModal.Main)}
+        onSuccess={() => setModal(CurrentModal.Main)}
+      />
+    );
+  }
+
+  return (
     <section className="bg-white dark:bg-gray-900 max-w-2xl rounded-lg px-8 py-8 flex-auto flex-col">
       <div className="mt-6 text-gray-500 dark:text-gray-400 text-base text-left">Your address</div>
       <div className="mt-1 text-gray-800 md:text-xl dark:text-white text-base text-left flex-col">
@@ -51,7 +56,7 @@ export function Main({ account }: MainProps) {
           Copy{' '}
         </button>
         <button
-          onClick={() => refresh()}
+          onClick={() => refetch()}
           className="px-3 py-1 ml-4 text-xs font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
         >
           Refresh
@@ -59,10 +64,10 @@ export function Main({ account }: MainProps) {
       </div>
       <p className="mt-16 text-gray-500 dark:text-gray-400 text-base text-left">Balance:</p>
       <div className="mt-1 text-3xl font-semibold tracking-wide text-left text-gray-800 md:text-3xl dark:text-white flex flex-col">
-        <div className="flex-none">{balance} aztecs</div>
+        <div className="flex-none">{data?.toString()} {TOKEN_LIST[0].symbol}</div>
       </div>
       <button
-        onClick={() => setShowModal(true)}
+        onClick={() => setModal(CurrentModal.SendTokens)}
         className="w-full px-6 py-2 mt-24 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
       >
         {' '}
