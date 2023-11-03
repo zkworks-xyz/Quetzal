b3d5c177af3768b48896ee7afa17f142db2f20be
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-11-03 14:24:44 +0100

    Error handling for sending tokens

--- src/app/modals/InfoDialog.tsx
+++ src/app/modals/InfoDialog.tsx
@@ -1,3 +1,5 @@
+import { PrimaryButton, SecondaryButton } from '../components/button.js';
+
 interface WaitDialogProps {
   title: string;
   message?: string;
@@ -15,6 +17,8 @@ export function InfoDialog({
   secondaryAction,
   secondaryLabel,
 }: WaitDialogProps) {
+  const isPrimary = primaryAction && primaryLabel;
+  const isSecondary = secondaryAction && secondaryLabel;
   return (
     <section className="bg-white dark:bg-gray-900 max-w-xl rounded-lg px-8 py-16">
       <div className="container flex flex-col items-center justify-center px-6 mx-auto">
@@ -22,24 +26,12 @@ export function InfoDialog({
           {title}
         </h1>
         {message && <p className="mt-6 text-gray-500 dark:text-gray-400 text-base text-left">{message}</p>}
-        <div className="flex items-center justify-center w-full mt-6 space-x-2">
-          {primaryAction && primaryLabel && (
-            <button
-              onClick={() => primaryAction()}
-              className="px-3 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-            >
-              {primaryLabel}
-            </button>
-          )}
-          {secondaryAction && secondaryAction && (
-            <button
-              onClick={() => secondaryAction()}
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            >
-              {secondaryLabel}
-            </button>
-          )}
-        </div>
+        {isPrimary && isSecondary && (
+          <div className="flex items-center justify-center w-full mt-6 space-x-2">
+            {isPrimary && <PrimaryButton label={primaryLabel} action={primaryAction} />}
+            {isSecondary && <SecondaryButton label={secondaryLabel} action={secondaryAction} />}
+          </div>
+        )}
       </div>
     </section>
   );
