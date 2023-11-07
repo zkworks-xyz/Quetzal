840cb9b35a8d0a2e81abcc34353166e588ebe59c
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-11-07 16:02:43 +0100

    Remove unused imports

--- src/tests/webauth.account.test.ts
+++ src/tests/webauth.account.test.ts
@@ -1,16 +1,10 @@
-import {
-  AccountManager,
-  AccountWalletWithPrivateKey,
-  PXE,
-  TxStatus,
-  isContractDeployed
-} from '@aztec/aztec.js';
+import { AccountManager, AccountWalletWithPrivateKey, PXE, TxStatus, isContractDeployed } from '@aztec/aztec.js';
 import { GrumpkinScalar } from '@aztec/circuits.js';
 import { TokenContract } from '@aztec/noir-contracts/types';
 import { beforeAll, describe, expect, it, jest } from '@jest/globals';
 import { getWebAuthnAccount } from '../app/account/webauthn_account_contract.js';
+import { setupSandbox } from './utils.js';
 import { WebAuthnInterfaceStub } from './webauthn_stub.js';
-import { setupSandbox } from '../app/account/utils.js';
 
 describe('Quetzal wallet', () => {
   jest.setTimeout(60_000);
