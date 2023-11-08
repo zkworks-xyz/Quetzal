e55ee833ebdff3e983db7b862dd1e34df1d5ac75
Arkadiusz Konior <akonior@gmail.com>
2023-11-08 12:16:49 +0100

    Move utils

--- src/tests/webauth.account.test.ts
+++ src/tests/webauth.account.test.ts
@@ -3,7 +3,7 @@ import { GrumpkinScalar } from '@aztec/circuits.js';
 import { TokenContract } from '@aztec/noir-contracts/types';
 import { beforeAll, describe, expect, it, jest } from '@jest/globals';
 import { getWebAuthnAccount } from '../app/account/webauthn_account_contract.js';
-import { setupSandbox } from './utils.js';
+import { setupSandbox } from '../app/account/utils.js';
 import { WebAuthnInterfaceStub } from './webauthn_stub.js';
 
 describe('Quetzal wallet', () => {
