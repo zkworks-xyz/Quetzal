d262584d1202bb12a02208088db3b8c243341111
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-11-08 15:29:37 +0100

    Add classes prop to all buttons

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -22,13 +22,14 @@ export function PrimaryButton({ label, action, classes = '' }: ButtonProps) {
   );
 }
 
-export function SecondaryButton({ label, action }: ButtonProps) {
+export function SecondaryButton({ label, action, classes = ''  }: ButtonProps) {
   return (
     <button
       onClick={() => action()}
       className={classNames(
         'inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1',
         'mt-4 ring-inset ring-gray-300 hover:bg-gray-50',
+        classes,
       )}
     >
       {label}
@@ -36,7 +37,7 @@ export function SecondaryButton({ label, action }: ButtonProps) {
   );
 }
 
-export function SmallButton({ label, action }: ButtonProps) {
+export function SmallButton({ label, action, classes = '' }: ButtonProps) {
   return (
     <button
       onClick={action}
@@ -44,6 +45,7 @@ export function SmallButton({ label, action }: ButtonProps) {
         'px-3 py-1 ml-4 text-xs font-medium tracking-wide text-white capitalize transition-colors',
         'duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring',
         'focus:ring-blue-300 focus:ring-opacity-50',
+        classes,
       )}
     >
       {label}
