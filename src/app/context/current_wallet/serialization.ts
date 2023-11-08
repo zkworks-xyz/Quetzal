91fc903bbcc8cbeedc00e584a704d5bb21cc3f41
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-11-08 14:55:17 +0100

    Introduce serialization to UserWallet

--- src/app/context/current_wallet/serialization.ts
+++ src/app/context/current_wallet/serialization.ts
@@ -6,28 +6,26 @@ import {
   DefaultAccountInterface,
   Fr,
   GrumpkinScalar,
+  NodeInfo,
   PXE,
   Point,
 } from '@aztec/aztec.js';
+import { UserWallet } from './UserWallet.js';
 
-export function serializeAccountWalletWithPrivateKey(wallet: AccountWalletWithPrivateKey): string {
+export function accountWalletWithPrivateKeyToObject(wallet: AccountWalletWithPrivateKey): object {
   const completeAddress = wallet.getCompleteAddress();
-  return JSON.stringify({
+  return {
     completeAddress: {
       address: completeAddress.address.toString(),
       publicKey: completeAddress.publicKey.toString(),
       partialAddress: completeAddress.partialAddress.toString(),
     },
     encryptionPrivateKey: wallet.getEncryptionPrivateKey().toString(),
-  });
+  };
 }
 
-export async function deserializeAccountWalletWithPrivateKey(
-  serialized: string,
-  pxe: PXE,
-  authWitnessProvider: AuthWitnessProvider,
-): Promise<AccountWalletWithPrivateKey> {
-  function reviver(key: string, value: any) {
+const reviver =
+  (pxe: PXE, authWitnessProvider: AuthWitnessProvider, nodeInfo: NodeInfo) => (key: string, value: any) => {
     if (key == 'completeAddress') {
       return new CompleteAddress(
         AztecAddress.fromString(value.address),
@@ -36,13 +34,31 @@ export async function deserializeAccountWalletWithPrivateKey(
       );
     } else if (key == 'encryptionPrivateKey') {
       return GrumpkinScalar.fromString(value);
-    } else if (key == '') {
+    } else if (key == 'wallet') {
       const account = new DefaultAccountInterface(authWitnessProvider, value.completeAddress, nodeInfo);
       return new AccountWalletWithPrivateKey(pxe, account, value.encryptionPrivateKey);
     }
     return value;
-  }
+  };
 
-  const nodeInfo = await pxe.getNodeInfo();
-  return JSON.parse(serialized, reviver);
+export function serializeUserWallet(userWallet: UserWallet): string {
+  return JSON.stringify({
+    name: userWallet.name,
+    wallet: accountWalletWithPrivateKeyToObject(userWallet.wallet),
+  });
+}
+
+export async function deserializeUserWallet(
+  json: string,
+  pxe: PXE,
+  authWitnessProvider: AuthWitnessProvider,
+): Promise<UserWallet> {
+  const userWallet = JSON.parse(json, reviver(pxe, authWitnessProvider, await pxe.getNodeInfo()));
+  if (!userWallet.name) {
+    throw new TypeError('Deserialization error: userWallet name is missing');
+  }
+  if (!(userWallet.wallet instanceof AccountWalletWithPrivateKey)) {
+    throw new TypeError('Deserialization error: wallet name is missing');
+  }
+  return userWallet;
 }
